76da09db1ff76efb9db55627ee3e1d3efb427358
jizuscreed <jizuscreed@gmail.com>
2017-05-10 01:22:21 +0300

    отлично, осталось только сделать работу с календарём и можно тестировать

--- src/lib/gitrepo.go
+++ src/lib/gitrepo.go
@@ -3,16 +3,20 @@ package lib
 import (
 	"os"
 	"log"
-	"fmt"
+	_ "fmt"
+	"errors"
+	"os/exec"
 )
 
 /**
  дескриптор репозитория гита
  */
 type gitrepo struct {
-	dir string
-	initialized bool
+	dir               string
+	initialized       bool
 	unCommitedChanges bool
+	lastCommitContent string
+	file              os.File
 }
 
 func (repo gitrepo) GetDir() string {
@@ -20,26 +24,79 @@ func (repo gitrepo) GetDir() string {
 }
 
 func (repo gitrepo) IsInitialized() bool {
-	return repo.initialized;
+	return repo.initialized
 }
 
 func (repo gitrepo) HasUnCommitedChanges() bool {
 	return repo.unCommitedChanges
 }
 
-func NewGitRepo(repoDir string) gitrepo {
-	currentDir, _ := getCurrentDir()
+func (repo *gitrepo) Init() error {
+	// так-с, проверяем, что репа ещё не инициализирована
+	if repo.initialized{
+		return errors.New("git repository in " + repo.dir + "is already initinalized")
+	}
 
-	// так-с, пиздуем в нужную директорию, если её не существует, то создаём
-	fileinfo, err := os.Stat(currentDir + "/../" + repoDir);
+	// отлично, инициализируемся
+	exec.Command("git", "init", repo.dir).Run()
 
-	if err != nil{
+	// теперь создаём файл
+	file, err := os.Create(repo.dir + "output.txt")
+	// закрываем файл
+	defer file.Close()
+	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println(fileinfo)
+	return nil
+}
 
-	repo := gitrepo{repoDir, false, false}
+func (repo *gitrepo) NewCommit(date string) error {
+	// меняем содержимое файла
+	file, err := os.OpenFile("output.txt", os.O_TRUNC|os.O_RDWR, 0777)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	// пишем туда
+	if repo.lastCommitContent == "+" {
+		_, err = file.Write([]byte("-"))
+	} else {
+		_, err = file.Write([]byte("+"))
+	}
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	// закрываем файл
+	file.Close()
+
+	// добавляем изменения в индекс (git add)
+	exec.Command("git", "add", repo.dir).Run()
+
+	// коммитимся (git commit -q --date "2017-01-01 12:00:02" -m "+")
+	exec.Command("git", "commit", repo.dir, "-q", "--date", "\"" + date + "\"", "-m", "\"+\"").Run()
+
+	return nil
+}
+
+func NewGitRepo(repoDir string) gitrepo {
+	currentDir, _ := getCurrentDir()
+	abspath := currentDir + "/../" + repoDir
+
+	// так-с, пиздуем в нужную директорию, если её не существует, то создаём
+	_, err := os.Stat(abspath)
+
+	if os.IsNotExist(err) {
+		// отлично, директории не существует - создаём её и работаем дальше
+		os.Mkdir(abspath, 0777)
+	} else if err != nil{
+		log.Fatal(err.Error())
+	} else {
+		// директория уже существует - бросеам фаталку
+		//log.Fatal("git repo directory " + repoDir + " is already exists") todo не забыть раскомметировать потом, мазафака
+	}
+
+	repo := gitrepo{abspath, false, false, "-", nil}
+	repo.Init()
 
 	return repo
 }
