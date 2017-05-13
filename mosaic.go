3ecbefbfa50bdbaa295a42b72742a726cb32c4a3
jizuscreed <jizuscreed@gmail.com>
2017-05-13 22:15:14 +0300

    bugfixes after testing 2

--- mosaic.go
+++ mosaic.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	_ "fmt"
 	"log"
 	"fmt"
-	_ "flag"
 	"os"
 	"errors"
 	"image"
@@ -14,18 +12,19 @@ import (
 	"time"
 	"strconv"
 )
-
-func getImagePath() (string, error) {
-	//var path string
-	if len(os.Args) > 1{
-		return os.Args[1], nil
+/**
+ получаем параметры выполнения программы
+ */
+func getFlags() (string, string, error) {
+	if len(os.Args) > 2{
+		return os.Args[1], os.Args[2], nil
 	} else {
-		return "", errors.New("")
+		return "", "", errors.New("")
 	}
 }
 
 /**
-честно стырено
+ инициализация
  */
 func init() {
 	// damn important or else At(), Bounds() functions will
@@ -34,19 +33,10 @@ func init() {
 	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
 }
 
-/**
-дальшнейшие действия :
-1) сделать всю работу с календарем (там оказывается совсем не сложно)
-+ 2) дописать создание коммитов
-+ 2,1) работа с файлом - писанина туда плючиков/минусиков, ёпта (с этого вообще стоило бы начать, а то это и для коммита надои для всего остального прочего)
-3) тестирование
-4) profit
- */
-
 func main() {
 
 	// получаем путь до картинки
-	imagePath, err := getImagePath()
+	imagePath, repoDir, err := getFlags()
 
 	if err != nil {
 		log.Fatal("Please, specify image file")
@@ -59,21 +49,19 @@ func main() {
 	img := lib.OpenFile(imagePath)
 	defer img.Close()
 
-	fmt.Println(img)
-
 	// так-с, запускаем анализ изображения
 	calendarData := lib.GetCalendarDataFromImage(img)
 
 	calendarResult := calendarData.Result()
 
 	// отлично, переходим к созданию репозитория гита, мать его
-	gitrepo := lib.NewGitRepo("test")
+	gitrepo := lib.NewGitRepo(repoDir)
 
 	// дальше получем дату на год назад и и топаем вперед до понедельника
 	date := time.Now()
 	date = date.AddDate(-1, 0, 0)
-	for date.Weekday().String() != "Monday" {
-		date = date.AddDate(0, 0, 1)
+	for date.Weekday().String() != "Sunday" {
+		date = date.AddDate(0, 0, -1)
 	}
 
 	// теперь округляем дату - отбрасываем нафиг часы, минуты и секунды
@@ -89,8 +77,8 @@ func main() {
 			tempDate := date // создаём временную дату, чтобы там изгаляться с секундами
 			// теперь делаем столько коммитов, сколько у нас процент заполненности дня
 			// (это для простоты, чтобы один процентаж не перегонять в другой)
-			for m := 0; m <calendarResult[i][l]; m++{
-				fmt.Println("commit " + strconv.Itoa(m))
+			for m := 0; m < calendarResult[i][l]; m++{
+				//fmt.Println("commit " + strconv.Itoa(m))
 				gitrepo.NewCommit(tempDate.Format("2006-01-02 15:04:05"))
 				tempDate = tempDate.Add(time.Second) // переводим временную дату на секунду вперед
 			}
@@ -98,6 +86,4 @@ func main() {
 			date = date.AddDate(0, 0, 1)
 		}
 	}
-
-	// по идее всё, можно тестировать
 }
